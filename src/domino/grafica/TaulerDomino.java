cea1a2f8b3e6b9f7d324d624a7713e549cfb9dc4
davidblancolopez <daviddbl94@gmail.com>
2017-04-24 20:12:11 +0200

    Cambios

--- src/domino/grafica/TaulerDomino.java
+++ src/domino/grafica/TaulerDomino.java
@@ -5,8 +5,10 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.util.ArrayDeque;
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -44,19 +46,29 @@ public class TaulerDomino extends javax.swing.JFrame {
         this.add(pOest, BorderLayout.WEST);
         this.add(pSud, BorderLayout.SOUTH);
 
-        
-        ImageIcon imatge = new ImageIcon("C:\\Users\\ALUMNEDAM\\Documents\\NetBeansProjects\\DOMINO-1-activitat-2 David Blanco\\domino-1-activitat-2\\Assets\\fitxa0-0.png");
+        fitxa1_1=new JButton();
+        ImageIcon imatge = new ImageIcon("C:\\Users\\ALUMNEDAM\\Documents\\NetBeansProjects\\DOMINO-1-activitat-2 David Blanco\\domino-1-activitat-2\\Assets\\Fitxa0-0.png");
         fitxa1_1.setIcon(imatge);
         
-        ImageIcon imatge2 = new ImageIcon("C:\\Users\\ALUMNEDAM\\Documents\\NetBeansProjects\\DOMINO-1-activitat-2 David Blanco\\domino-1-activitat-2\\Assets\\fitxa0-1.png");
+        
+        fitxa1_2=new JButton();
+        ImageIcon imatge2 = new ImageIcon("C:\\Users\\ALUMNEDAM\\Documents\\NetBeansProjects\\DOMINO-1-activitat-2 David Blanco\\domino-1-activitat-2\\Assets\\Fitxa0-1.png");
         fitxa1_2.setIcon(imatge2);
         
-        ImageIcon imatge3 = new ImageIcon("C:\\Users\\ALUMNEDAM\\Documents\\NetBeansProjects\\DOMINO-1-activitat-2 David Blanco\\domino-1-activitat-2\\Assets\\fitxa0-2.png");
+        fitxa1_3=new JButton();
+        fitxa1_4=new JButton();
+        fitxa1_5=new JButton();
+        fitxa1_6=new JButton();
+        fitxa1_7=new JButton();
+
+        ImageIcon imatge3 = new ImageIcon("C:\\Users\\ALUMNEDAM\\Documents\\NetBeansProjects\\DOMINO-1-activitat-2 David Blanco\\domino-1-activitat-2\\Assets\\Fitxa0-2.png");
         fitxa1_2.setIcon(imatge3);
         fitxa1_4.setIcon(imatge3);
         fitxa1_5.setIcon(imatge3);
         fitxa1_6.setIcon(imatge3);
         fitxa1_7.setIcon(imatge3);
+        
+        fitxesJug1 = new ArrayDeque<JButton>();
         
         fitxesJug1.add(fitxa1_1);
         fitxesJug1.add(fitxa1_2);
@@ -65,6 +77,14 @@ public class TaulerDomino extends javax.swing.JFrame {
         fitxesJug1.add(fitxa1_5);
         fitxesJug1.add(fitxa1_6);
         fitxesJug1.add(fitxa1_7);
+              
+        
+        pOest.setLayout(new GridLayout(1, 7));
+        this.add(fitxa1_1);
+        this.add(fitxa1_2);
+        this.add(fitxa1_3);
+        this.add(fitxa1_4);
+        this.add(fitxa1_5);
         
         pNord.setBackground(Color.red);
         pSud.setBackground(Color.GREEN);
